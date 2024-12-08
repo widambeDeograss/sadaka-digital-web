1c76e53e8a126eae015781f7461f65ba808ab050
DEOGASS WIDAMBE <87471672+widambeDeograss@users.noreply.github.com>
2024-12-08 09:16:27 +0300

    Update base URL for development: switch to localhost API endpoint in BaseUrl.ts

--- src/Constants/BaseUrl.ts
+++ src/Constants/BaseUrl.ts
@@ -1,2 +1,2 @@
-export const DataBaseUrl =  "http://143.198.212.191:8000/api/v1";
-// export const DataBaseUrl =  "http://localhost:8000/api/v1";
+// export const DataBaseUrl =  "http://143.198.212.191:8000/api/v1";
+export const DataBaseUrl =  "http://localhost:8000/api/v1";
