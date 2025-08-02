260a996b9f16be4af806c813b5603d153e5a3cf4
widambeDeograss <widambedeograss5@gmail.com>
2025-08-02 22:39:13 +0300

    Update BaseUrl to switch to production API endpoint

--- src/Constants/BaseUrl.ts
+++ src/Constants/BaseUrl.ts
@@ -1,2 +1,2 @@
-export const DataBaseUrl =  "http://localhost:8000/api/v1";
-// export const DataBaseUrl =  "https://api.bmcmakabe.or.tz/api/v1";
+// export const DataBaseUrl =  "http://localhost:8000/api/v1";
+export const DataBaseUrl =  "https://api.bmcmakabe.or.tz/api/v1";
